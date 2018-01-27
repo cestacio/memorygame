e94d132c0df661b473a2d8fe9a058897912e5a29
Charmaine Estacio <charmaine.estacio@gmail.com>
2018-01-26 23:15:17 -0800

    updated notes for functions

--- app.js
+++ app.js
@@ -4,59 +4,71 @@ var cardOpened = "";
 var imageOpened = "";
 var imageFound = 0;
 
-var cardValues = [
-"https://i.imgur.com/yZnjsuD.jpg?1",
-"https://i.imgur.com/H4caeX8.png",
-"https://i.imgur.com/wYGcIr2.jpg"
-"https://i.imgur.com/ubGixv2.jpg?1",
-"https://i.imgur.com/HcSbU6P.jpg?1",
-"https://i.imgur.com/cQ9kl0k.jpg?1",
-"https://i.imgur.com/3IBqx9c.jpg?1",
-"https://i.imgur.com/X7iddbP.jpg?1",
-"https://i.imgur.com/ScLbTqC.png?1",
-"https://i.imgur.com/LcecYne.jpg?1",
-"https://i.imgur.com/1EBEoGx.png?1",
-"https://i.imgur.com/Vcz4rtz.gif?1"
+var cards = [
+    "https://i.imgur.com/Vcz4rtzundefined.gif?2",
+    "https://i.imgur.com/1EBEoGxundefined.png?1",
+    "https://i.imgur.com/cQ9kl0kundefined.jpg?1",
+    "https://i.imgur.com/3IBqx9cundefined.jpg?1",
+    "https://i.imgur.com/X7iddbPundefined.jpg?1",
+    "https://i.imgur.com/wYGcIr2undefined.jpg",
+    "https://i.imgur.com/ubGixv2undefined.jpg?1",
+    "https://i.imgur.com/HcSbU6Pundefined.jpg?1",
+    "https://i.imgur.com/LcecYneundefined.jpg?1",
+    "https://i.imgur.com/ScLbTqCundefined.png?1",
+    "https://i.imgur.com/H4caeX8undefined.png",
+    "https://i.imgur.com/yZnjsuDundefined.jpg?1",
+    "https://i.imgur.com/Vcz4rtzundefined.gif?2",
+    "https://i.imgur.com/1EBEoGxundefined.png?1",
+    "https://i.imgur.com/cQ9kl0kundefined.jpg?1",
+    "https://i.imgur.com/3IBqx9cundefined.jpg?1",
+    "https://i.imgur.com/X7iddbPundefined.jpg?1",
+    "https://i.imgur.com/wYGcIr2undefined.jpg",
+    "https://i.imgur.com/ubGixv2undefined.jpg?1",
+    "https://i.imgur.com/HcSbU6Pundefined.jpg?1",
+    "https://i.imgur.com/LcecYneundefined.jpg?1",
+    "https://i.imgur.com/ScLbTqCundefined.png?1",
+    "https://i.imgur.com/H4caeX8undefined.png",
 ];
 
-// load images
-$(cardValues).each(function() {
-	var image = $('<img />').attr('src', this);
-});
+/* load images
+$(cards).each(function() {
+	var image = $("<img />").attr("src", this);
+});*/
 
-// create newGame button
-function newGame() {
-	shuffleCards();
-	$(gameBoard + " div img").hide();
-	$(gameBoard + " div").css("visibility", "visibile");
-
-
-}
-
-// flip card
-function flipCard() {
-
-}
-
-
-// randomize array
-function randomize(max, min) {
-	return Math.round(Math.random() * (max - min) + min);
-}
+// click start new game
 
 // shuffle cards
 function shuffleCards() {
-	var currentIndex = cardValues.length, temp, randomIndex;
+    var randomIndex = 0;
+    var temp = 0;
 
-	while(0 !== currentIndex) {
-
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= -1;
-
-		temp = cardValues[currentIndex];
-		cardValues[currentIndex] = cardValues[randomIndex];
-		cardValues[randomIndex] = temp;
-
-	}
-	return cardValues;
+    for (var i = 0; i < cards.length; i++) {
+        randomIndex = Math.floor(Math.random() * i);
+        temp = cards[i];
+        cards[i] = cards[randomIndex];
+        cards[randomIndex] = temp;
+    }
+    assignCards();
 }
+
+function assignCards() {
+    $('.card').each(function(index) {
+        $(this).attr(data - card - )
+    })
+}
+
+// flip card
+function flipCard() {}
+
+// keep track of number of clicks
+
+// store lowest scoring game in local storage so that players can see record of 
+// best game played
+
+// create newGame button
+function newGame() {
+    shuffleCards();
+    $(gameBoard + " div img").hide();
+    $(gameBoard + " div").css("visibility", "visibile");
+}
+
